f9b124a7ffa600eec38e7afff96b341b2a869d76
DESKTOP-PIS374D\79537 <aleksander.conushin@yandex.ru>
2021-09-15 22:59:36 +0300

    Create first tutorial

---
+++ Assets/Scripts/Managers/ManagerTutorial.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class ManagerTutorial : Singleton<ManagerTutorial>
+{
+    [SerializeField] private TutorSwipeTap tutorSwipeTap;
+
+    private int[] levelsWithTutor = new int[5] { 0, 1, 2, 3, 4 };
+
+    public void CheckLevel(int number)
+    {
+        foreach (var numberLevel in levelsWithTutor)
+        {
+            if(number == numberLevel)
+            {
+                StartTutor(number);
+            }
+        }
+    }
+
+    private void StartTutor(int number)
+    {
+        if(number == 0)
+        {
+            tutorSwipeTap.StartTutor();
+        }
+    }
+}
